8ccbf2d2360d66c6cdcd3e2ea805bf2adbc36364
Christopher Ta <39348633+Digital1O1@users.noreply.github.com>
2021-07-21 18:40:54 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,60 @@
-const int buttonPin  = 4;     // the pin that the pushbutton is attached to
-const int ledPin     = 13;    // the pin that the LED is attached to
+#include <Arduino.h>
+const int buttonPin = 4; // the pin that the pushbutton is attached to
+const int ledPin = 13;   // the pin that the LED is attached to
 
-int buttonState      = 0;     // current state of the button
-int lastButtonState  = 0;     // previous state of the button
-int ledState         = 0;     // remember current led state
+int buttonState = 0;     // current state of the button
+int lastButtonState = 0; // previous state of the button
+int ledState = 0;        // remember current led state
 int relay_pin = 3;
+
+const unsigned long debounce_Interval = 20;
+unsigned long previousTime = 0;
 
 void setup()
 {
-  pinMode(buttonPin, INPUT);  // initialize the button pin as a input
-  pinMode(relay_pin, OUTPUT);  // initialize the button pin as a input
-  pinMode(LED_BUILTIN, OUTPUT);    // initialize the button pin as a output
+  pinMode(buttonPin, INPUT);    // initialize the button pin as a input
+  pinMode(relay_pin, OUTPUT);   // initialize the button pin as a input
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the button pin as a output
 
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(relay_pin, LOW);  
+  digitalWrite(relay_pin, LOW);
 }
 
 void loop()
 {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState != lastButtonState)
+  unsigned long currentTime = millis();
+
+  if (currentTime - previousTime >= debounce_Interval)
   {
 
-    // change the state of the led when someone pressed the button
-    if (buttonState == 1)
+    buttonState = digitalRead(buttonPin);
+    if (buttonState != lastButtonState)
     {
-      if (ledState == 1)
+
+      // change the state of the led when someone pressed the button
+      if (buttonState == 1)
       {
-        ledState = 0;
+        if (ledState == 1)
+        {
+          ledState = 0;
+        }
+        else
+        {
+          ledState = 1;
+        }
       }
-      else
-      {
-        ledState = 1;
-      }
+
+      // remember the current state of the button
+      lastButtonState = buttonState;
     }
 
-    // remember the current state of the button
-    lastButtonState = buttonState;
+    // turns LED on if the ledState=1 or off if the ledState=0
+    digitalWrite(LED_BUILTIN, ledState);
+    digitalWrite(relay_pin, ledState);
+
+    // adding a small delay prevents reading the buttonState to fast
+    // ( debouncing )
+    //delay(20);
+    previousTime = currentTime;
   }
-
-  // turns LED on if the ledState=1 or off if the ledState=0
-  digitalWrite(LED_BUILTIN, ledState);
-  digitalWrite(relay_pin, ledState);
-
-  // adding a small delay prevents reading the buttonState to fast
-  // ( debouncing )
-  delay(20);
 }
